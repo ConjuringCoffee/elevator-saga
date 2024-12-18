dd5b9d38cf9bbec57971b7c2ea4957285fea7868
ConjuringCoffee <72548231+ConjuringCoffee@users.noreply.github.com>
2024-12-18 11:44:55 +0100

    Solve challenge 1 (#1)
    * Add type definitions
    
    * Add linting
    
    * Add very simple solution

--- challenge_1.js
+++ challenge_1.js
@@ -1,15 +1,28 @@
-{
-    init: function(elevators, floors) {
-        var elevator = elevators[0]; // Let's use the first elevator
+// Challenge #1: Transport 15 people in 60 seconds or less
+// Three floors: 0-2
+// One elevator
 
-        // Whenever the elevator is idle (has no more queued destinations) ...
-        elevator.on("idle", function () {
-            // let's go to all the floors (or did we forget one?)
-            elevator.goToFloor(0);
-            elevator.goToFloor(1);
+/// <reference path="./elevatorsaga.d.ts" />
+/** @type {Game} */
+({
+    init: function (elevators, floors) {
+        // Let's only use the first elevator for now
+        const elevator = elevators[0];
+
+        elevator.on("floor_button_pressed", (floorNumber) => {
+            elevator.goToFloor(floorNumber);
         });
+
+        floors.forEach((floor) => {
+            floor.on("up_button_pressed", () => {
+                elevator.goToFloor(floor.floorNum());
+            });
+            floor.on("down_button_pressed", () => {
+                elevator.goToFloor(floor.floorNum());
+            });
+        })
     },
-    update: function(dt, elevators, floors) {
-        // We normally don't need to do anything here
+    update: function (dt, elevators, floors) {
+        // Do nothing for now
     }
-}
+})
