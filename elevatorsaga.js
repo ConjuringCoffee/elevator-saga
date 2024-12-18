c0a36058d8c021539447b00f3c111b92f326b9ed
ConjuringCoffee <72548231+ConjuringCoffee@users.noreply.github.com>
2024-12-18 17:11:26 +0100

    Improve finding nearest elevator

--- elevatorsaga.js
+++ elevatorsaga.js
@@ -2,15 +2,18 @@
 /** @type {Game} */
 ({
     init: function (elevators, floors) {
-        const findAvailableElevator = (
+        const searchAvailableElevator = (
             /** @type {number} */
             floorNumber
         ) => {
+            console.debug('\n');
+            console.debug(`Searching best elevator to pick up from floor ${floorNumber}`);
+
             /** @type {Elevator | null} */
             var bestElevator = null;
             var bestDistance = 99999999999;
 
-            const setBestBasedOnDistance = (/** @type {Elevator} */ elevator) => {
+            const setBestIfCurrentDistanceIsBest = (/** @type {Elevator} */ elevator) => {
                 const distance = Math.floor(elevator.currentFloor() - floorNumber);
                 if (distance < bestDistance) {
                     bestElevator = elevator;
@@ -18,7 +21,29 @@
                 };
             }
 
-            const isElevatorMovingTowardsFloor = (/** @type {Elevator} */ elevator) => {
+            const getShortestFutureDistance = (/** @type {Elevator} */ elevator) => {
+                var shortestFutureDistance = 99999999999;
+
+                elevator.destinationQueue.forEach((destination) => {
+                    const distance = Math.floor(destination - floorNumber);
+
+                    if (distance < shortestFutureDistance) {
+                        shortestFutureDistance = distance;
+                    };
+                })
+
+                return shortestFutureDistance;
+            }
+
+            const setBestIfFutureDistanceIsBest = (/** @type {Elevator} */ elevator) => {
+                const distance = getShortestFutureDistance(elevator);
+                if (distance < bestDistance) {
+                    bestElevator = elevator;
+                    bestDistance = distance;
+                };
+            }
+
+            const isElevatorMovingTowardsOrStoppedOnFloor = (/** @type {Elevator} */ elevator) => {
                 // TODO: Handle edge case: Rounded currentFloor() could mean that the floor was already passed
                 const distance = elevator.currentFloor() - floorNumber;
 
@@ -26,22 +51,33 @@
                     return true;
                 } else if (distance <= 0 && elevator.destinationDirection() === "up") {
                     return true;
+                } else if (distance === 0 && elevator.destinationDirection() === "stopped") {
+                    return true;
                 } else {
                     return false;
                 }
             }
 
             elevators.forEach((elevator) => {
-                if (isElevatorMovingTowardsFloor(elevator)) {
-                    setBestBasedOnDistance(elevator);
+                if (isElevatorMovingTowardsOrStoppedOnFloor(elevator)) {
+                    setBestIfCurrentDistanceIsBest(elevator);
                 }
             });
 
             if (bestElevator === null) {
-                // TODO: Improve fallback logic
-                console.warn('No available elevator found, choosing the first one as fallback');
+                console.debug('No elevator moving towards the floor, so disregard the direction');
+
+                elevators.forEach((elevator) => {
+                    setBestIfFutureDistanceIsBest(elevator);
+                });
+            }
+
+            if (bestElevator === null) {
+                console.error('No elevator found, use first elevator as fallback');
                 bestElevator = elevators[0];
             }
+
+            console.debug(`E${bestElevator._index}: Chosen as best elevator`);
 
             return bestElevator;
         }
@@ -52,6 +88,7 @@
             /** @type {number} */
             floorNumber
         ) => {
+            console.debug('\n');
             console.debug(`E${elevator._index}: Inserting ${floorNumber} as new destination`);
             console.debug(`E${elevator._index}: Current floor ${elevator.currentFloor()}, direction ${elevator.destinationDirection()}`);
             console.debug(`E${elevator._index}: Current destination queue:`, elevator.destinationQueue.toString());
@@ -105,12 +142,13 @@
         });
 
         floors.forEach((floor) => {
+            // TODO: Include up / down button in decision on which elevator to send
             floor.on("up_button_pressed", () => {
-                const elevator = findAvailableElevator(floor.floorNum());
+                const elevator = searchAvailableElevator(floor.floorNum());
                 insertDestination(elevator, floor.floorNum());
             });
             floor.on("down_button_pressed", () => {
-                const elevator = findAvailableElevator(floor.floorNum());
+                const elevator = searchAvailableElevator(floor.floorNum());
                 insertDestination(elevator, floor.floorNum());
             });
         })
