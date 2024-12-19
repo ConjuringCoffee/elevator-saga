7fe76032f12275f1de17e87a99662ef93dcb3016
ConjuringCoffee <72548231+ConjuringCoffee@users.noreply.github.com>
2024-12-19 09:20:10 +0100

    Use TypeScript for unit tests

--- elevatorsaga.test.ts
+++ elevatorsaga.test.ts
@@ -1,30 +1,31 @@
 const fs = require('fs');
+const data = fs.readFileSync('./elevatorsaga.js', 'utf8');
 
-test('First test', () => {
+var mockElevators: Array<Partial<Elevator>> = [];
+var mockFloors: Array<Partial<Floor>> = [];
 
+beforeEach(() => {
     const createElevator = () => {
         return {
             on: () => { },
         };
     }
 
-    const createFloor = (/** @type {number} */ floorNumber) => {
+    const createFloor = (floorNumber: number) => {
         return {
             floorNum: () => floorNumber,
             on: () => { }
         };
     };
 
-    const mockElevators = [createElevator(), createElevator()];
-    const mockFloors = [createFloor(0), createFloor(1)];
+    mockElevators = [createElevator(), createElevator()];
+    mockFloors = [createFloor(0), createFloor(1)];
+})
 
-    const data = fs.readFileSync('./elevatorsaga.js', 'utf8');
-
+test('All elevators get assigned to an index', () => {
     const game = eval(data);
     game.init(mockElevators, mockFloors);
 
-    // @ts-ignore
     expect(mockElevators[0]._index).toBe(0);
-    // @ts-ignore
     expect(mockElevators[1]._index).toBe(1);
 });
